b74ed67e6010d8eb6f4439e60ac7165cbea38996
Saul <saulvargascosta@hotmail.com>
2024-03-25 20:37:02 -0300

    Lista alunos + bug icones

---
+++ src/serviços/ListaAlunos.js
@@ -0,0 +1,24 @@
+const ListaAlunos = [
+    { nome: 'João da Silva', matricula: '001', celular: '(11) 9 8765-4321' },
+    { nome: 'Maria Oliveira', matricula: '002', celular: '(11) 9 8765-4322' },
+    { nome: 'Pedro Santos', matricula: '003', celular: '(11) 9 8765-4323' },
+    { nome: 'Ana Costa', matricula: '004', celular: '(11) 9 8765-4324' },
+    { nome: 'Lucas Pereira', matricula: '005', celular: '(11) 9 8765-4325' },
+    { nome: 'Juliana Souza', matricula: '006', celular: '(11) 9 8765-4326' },
+    { nome: 'Bruno Lima', matricula: '007', celular: '(11) 9 8765-4327' },
+    { nome: 'Camila Fernandes', matricula: '008', celular: '(11) 9 8765-4328' },
+    { nome: 'Guilherme Almeida', matricula: '009', celular: '(11) 9 8765-4329' },
+    { nome: 'Larissa Gomes', matricula: '010', celular: '(11) 9 8765-4330' },
+    { nome: 'Rafael Martins', matricula: '011', celular: '(11) 9 8765-4331' },
+    { nome: 'Paula Oliveira', matricula: '012', celular: '(11) 9 8765-4332' },
+    { nome: 'Thiago Silva', matricula: '013', celular: '(11) 9 8765-4333' },
+    { nome: 'Marina Vieira', matricula: '014', celular: '(11) 9 8765-4334' },
+    { nome: 'Carlos Rodrigues', matricula: '015', celular: '(11) 9 8765-4335' },
+    { nome: 'Fernanda Costa', matricula: '016', celular: '(11) 9 8765-4336' },
+    { nome: 'Diego Oliveira', matricula: '017', celular: '(11) 9 8765-4337' },
+    { nome: 'Isabela Sousa', matricula: '018', celular: '(11) 9 8765-4338' },
+    { nome: 'Matheus Lima', matricula: '019', celular: '(11) 9 8765-4339' },
+    { nome: 'Vanessa Santos', matricula: '020', celular: '(11) 9 8765-4340' }
+];
+
+export default ListaAlunos;
--- src/telas/DashScreen.js
+++ src/telas/DashScreen.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { ScrollView, StyleSheet, Text, View } from 'react-native'
 import { Button } from '../componentes/Button'
 import { useNavigation } from '@react-navigation/native'
+import ListaAlunos from '../serviços/ListaAlunos'
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 export const DashScreen = () => {
 
@@ -11,36 +13,13 @@ export const DashScreen = () => {
         navigation.navigate('ScanerScreen')
     }
 
-    const alunos = [
-        { nome: 'João da Silva', matricula: '001', celular: '(11) 9 8765-4321' },
-        { nome: 'Maria Oliveira', matricula: '002', celular: '(11) 9 8765-4322' },
-        { nome: 'Pedro Santos', matricula: '003', celular: '(11) 9 8765-4323' },
-        { nome: 'Ana Costa', matricula: '004', celular: '(11) 9 8765-4324' },
-        { nome: 'Lucas Pereira', matricula: '005', celular: '(11) 9 8765-4325' },
-        { nome: 'Juliana Souza', matricula: '006', celular: '(11) 9 8765-4326' },
-        { nome: 'Bruno Lima', matricula: '007', celular: '(11) 9 8765-4327' },
-        { nome: 'Camila Fernandes', matricula: '008', celular: '(11) 9 8765-4328' },
-        { nome: 'Guilherme Almeida', matricula: '009', celular: '(11) 9 8765-4329' },
-        { nome: 'Larissa Gomes', matricula: '010', celular: '(11) 9 8765-4330' },
-        { nome: 'Rafael Martins', matricula: '011', celular: '(11) 9 8765-4331' },
-        { nome: 'Paula Oliveira', matricula: '012', celular: '(11) 9 8765-4332' },
-        { nome: 'Thiago Silva', matricula: '013', celular: '(11) 9 8765-4333' },
-        { nome: 'Marina Vieira', matricula: '014', celular: '(11) 9 8765-4334' },
-        { nome: 'Carlos Rodrigues', matricula: '015', celular: '(11) 9 8765-4335' },
-        { nome: 'Fernanda Costa', matricula: '016', celular: '(11) 9 8765-4336' },
-        { nome: 'Diego Oliveira', matricula: '017', celular: '(11) 9 8765-4337' },
-        { nome: 'Isabela Sousa', matricula: '018', celular: '(11) 9 8765-4338' },
-        { nome: 'Matheus Lima', matricula: '019', celular: '(11) 9 8765-4339' },
-        { nome: 'Vanessa Santos', matricula: '020', celular: '(11) 9 8765-4340' }
-    ];
-
     return (
         <View style={styles.container}>
             <View style={styles.viewTilteHeade}><Text style={styles.titleHeader}>Disciplina: Engenharia de Software 2</Text></View>
-            <View style={styles.viewTilteLista}><Text style={styles.titleLista}>ALunos</Text></View>
+            <View style={styles.viewTilteLista}><Text style={styles.titleLista}>Alunos</Text></View>
             <View style={styles.viewScrollView}>
                 <ScrollView>
-                    {alunos.map((item, index) => (
+                    {ListaAlunos.map((item, index) => (
                         <View key={index} style={styles.viewListaAluno} >
                             <View style={styles.viewItem}>
                                 <Text style={styles.textListaAluno}>{item.nome} - {item.matricula} - {item.celular}</Text>
@@ -49,6 +28,16 @@ export const DashScreen = () => {
                     ))}
                 </ScrollView>
             </View>
+
+            <View style={styles.viewLegenda}>
+                <Text>Legenda</Text>
+                <View style={styles.viewLegendasIcon}>
+                    <Text>Presença: <Icon name="check" size={20} color="green" /></Text>
+                    <Text>Justificativa:</Text>
+                    <Text>Falta:</Text>
+                </View>
+            </View>
+
             <View style={styles.viewBtn}>
                 <Button text='Realizar Cham.' onPress={handleChamada} />
             </View>
@@ -108,5 +97,15 @@ const styles = StyleSheet.create({
         borderBottomWidth: 2,
         width: '100%'
     },
-    
+    viewLegendasIcon:{
+        flexDirection:'row',
+        justifyContent:"space-between",
+        width: '60%',
+        paddingTop: 10
+    },
+    viewLegenda:{
+        alignItems: 'center',
+        paddingTop: 10
+    }
+
 })
